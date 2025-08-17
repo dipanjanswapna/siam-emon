d3a88b87fdf75efe629835b01fdc91082e86a634
Prangon Rodi <rodiprangon@gmail.com>
2025-08-17 20:59:30 +0000

    full web app jure nicher font beboher koro

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Alegreya', 'serif'],
-        headline: ['Belleza', 'sans-serif'],
+        body: ['"Hind Siliguri"', 'sans-serif'],
+        headline: ['"Hind Siliguri"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
