63a3f3f92cbb0e029732fa0e30fdf6b12b78a997
Prangon Rodi <rodiprangon@gmail.com>
2025-08-18 14:57:23 +0000

    <!-- Google tag (gtag.js) --> <script async src="https://www.googletagma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Footer } from '@/components/layout/footer';
 import { Toaster } from '@/components/ui/toaster';
 import { FloatingFeedbackButton } from '@/components/layout/FloatingFeedbackButton';
 import Image from 'next/image';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: {
@@ -69,6 +70,16 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;700&display=swap" rel="stylesheet" />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-TS85T6ZB8Q"></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-TS85T6ZB8Q');
+          `}
+        </Script>
       </head>
       <body className={cn("font-body antialiased min-h-screen flex flex-col")}>
         <Header />
